b39995a2584e5eb96b0ae313af67d514355e8670
Danny G Smith <dgs@riskiii.com>
2017-10-07 21:47:26 -0500

    initial commit

---
+++ bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Carbon Fields Engine Plugin
+ *
+ * @package     CampFirePixels\CarbonFields
+ * @author      Danny G Smith
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Carbon Fields Engine Plugin
+ * Plugin URI:  https://github.com/dannygsmith/carbon-fields-engine.git
+ * Description: Barebones sandbox Testing plugin where we can write code, reverse engineer, and break stuff to learn deeply about WordPress web development.
+ * Version:     1.0.0
+ * Author:      hellofromTonya
+ * Author URI:  https://KnowTheCode.io
+ * Text Domain: sandbox
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+namespace KnowTheCode\BarebonesSandbox;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Cheatin&#8217; uh?' );
+}
+
+// add your cool testing and playing code here.  Have fun!
